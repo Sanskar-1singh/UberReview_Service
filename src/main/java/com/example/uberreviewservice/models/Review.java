986be1a204e6de8e7a4fa811efdbfbbe391e83dd
Sanskar-1singh <sanskarsingh812@gmail.com>
2025-11-06 17:43:19 +0530

    Added lombok getter and setter

--- src/main/java/com/example/uberreviewservice/models/Review.java
+++ src/main/java/com/example/uberreviewservice/models/Review.java
@@ -1,18 +1,41 @@
 package com.example.uberreviewservice.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+@Table(name = "booking_review")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter//only work at database level>>>
 public class Review {
-
     @Id//this annotation makes the id property as primary key of my table
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
 
+    @Column(nullable = false)
+   private String content;
+
+
+    @Column(nullable = false)
+   private Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+   private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
 
 
 
